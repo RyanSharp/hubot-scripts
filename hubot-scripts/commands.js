d7fd80184cd51ddd1e7cf60c47fb4b297225c0e5
ryansharp <rtsharp90@gmail.com>
2017-03-15 18:02:50 +0000

    breaking out roll commands into separate file, including config file and moving sensitive data to hidden.js

--- hubot-scripts/commands.js
+++ hubot-scripts/commands.js
@@ -1,39 +1,4 @@
 const config = require("./config");
-var pos = require("pos");
-
-const sentence = "I open the box";
-var words = new pos.Lexer().lex(sentence);
-console.log(words);
-var tagger = new pos.Tagger();
-console.log(tagger.tag(words));
-
-const IMPRESSIVE_ACTIONS = [
-    "holding and not spilling his beer",
-    "doing a back flip",
-    "eating a hot pocket, without letting it cool down",
-    "cooking the perfect steak",
-    "walking on water",
-    "hacking into the mainframe",
-];
-
-function randomImpressiveAction() {
-    return IMPRESSIVE_ACTIONS[Math.floor(Math.random() * IMPRESSIVE_ACTIONS.length)];
-}
-
-const PATHETIC_ACTIONS = [
-    "choked on a fly that happened to be in the wrong place at the wrong time",
-    "missed the bus.  Now they're still waiting for the next one.",
-    "gave up.  It was just way too hard",
-    "realized that it just wasn't their calling.  It's really admirable that you understand that you're just not very good at most things.",
-    "forgot what they were going to do",
-    "just couldn't figure out",
-    "got lost, like wtf.",
-    "died of dysntery",
-];
-
-function randomPatheticAction() {
-    return PATHETIC_ACTIONS[Math.floor(Math.random() * PATHETIC_ACTIONS.length)];
-}
 
 module.exports = function(robot) {
     robot.hear(/(swiggity swooty|swiggityswooty|swiggityswoogity|swiggity swoogity)/, function(msg) {
@@ -52,60 +17,6 @@ module.exports = function(robot) {
     });
     robot.hear(/(whats for lunch|what's for lunch|Whats for lunch|lunch)/, function(msg) {
         msg.send(config.lunch_options[Math.floor(config.lunch_options.length * Math.random())]);
-    });
-    robot.hear(/^roll$/, function(msg) {
-        var roll_result = (Math.floor(Math.random() * 20) + 1);
-        if (roll_result === 20) {
-            msg.send(msg.message.user.name + " rolled a natural 20, fuck.");
-        }
-        else if (roll_result === 1) {
-            msg.send("Critical miss, lol. " + msg.message.user.name + " rolled a 1.");
-        }
-        else if (roll_result < 6) {
-            msg.send(msg.message.user.name + " rolled a disappointing " + roll_result);
-        }
-        else if (roll_result > 14) {
-            msg.send(msg.message.user.name + " rolled a pretty fucking nice " + roll_result);
-        }
-        else {
-            msg.send(msg.message.user.name + " rolled a " + roll_result);
-        }
-    });
-    robot.hear(/^roll d(4|6|8|10|12|20)$/, function(msg) {
-        var ceiling = Number(msg.match[1]);
-        var roll_result = (Math.floor(Math.random() * ceiling) + 1);
-        console.log(roll_result);
-        if (roll_result === ceiling) {
-            msg.send(msg.message.user.name + " rolled a fucking natural " + ceiling + "!");
-        }
-        else if (roll_result === 1) {
-            msg.send("Critical miss. " + msg.message.user.name + " rolled a 1, loser.");
-        }
-        else {
-            msg.send(msg.message.user.name + " rolled a " + roll_result);
-        }
-    });
-    robot.hear(/tries to (.*)/, function(msg) {
-        var roll_result = (Math.floor(Math.random() * 20) + 1);
-        if (msg.message.user.name === 'weston') {
-            var message = msg.message.user.name + "rolls a 1\n";
-            message += "On " + msg.message.user.name + "'s way to " + msg.match[1] + ", " + msg.message.user.name + " " + randomPatheticAction();
-        } else {
-            var message = msg.message.user.name + "rolls a " + roll_result + "\n";
-            if (roll_result === 20) {
-                message += msg.message.user.name + " fucking " + msg.match[1] + ".\n";
-            } else if (roll_result > 15) {
-                message += "Not only does " + msg.message.user.name + " successfully " + msg.match[1] + ".\n";
-                message += msg.message.user.name + " does it while also " + randomImpressiveAction() + " and " + randomImpressiveAction();
-            } else if (roll_result > 12) {
-                message += msg.message.user.name + " does it and makes it look easy."
-            } else if (roll_result > 7) {
-                message += "Sorry, " + msg.message.user.name + ", but you'll need a little bit more practice before you can " + msg.match[1];
-            } else {
-                message += "On " + msg.message.user.name + "'s way to " + msg.match[1] + ", " + msg.message.user.name + " " + randomPatheticAction();
-            }
-        }
-        msg.send(message);
     });
     robot.hear(/set home (.*)/, function(msg) {
         msg.http(config.api_url + "/api/set_home").query({user: msg.message.user.name, address: msg.match[1]})
